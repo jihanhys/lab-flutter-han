cdd01951aeb8d3dbbed50a119fc708922fe2ff7d
jihanifah <jihanhanifahyasmin12@gmail.com>
2022-11-17 13:35:00 +0700

    finished tutorial 8 - han

--- lib/page/form.dart
+++ lib/page/form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:labpbphan/main.dart';
+import 'package:labpbphan/page/to_do_page.dart';
 
 class MyFormPage extends StatefulWidget {
     const MyFormPage({super.key});
@@ -36,26 +37,36 @@ class _MyFormPageState extends State<MyFormPage> {
                 child: Column(
                     children: [
                     // Menambahkan clickable menu
-                    ListTile(
-                        title: const Text('Counter'),
-                        onTap: () {
-                        // Route menu ke halaman utama
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(builder: (context) => const MyHomePage()),
-                        );
-                        },
-                    ),
-                    ListTile(
-                        title: const Text('Form'),
-                        onTap: () {
-                        // Route menu ke halaman form
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(builder: (context) => const MyFormPage()),
-                        );
-                        },
-                    ),
+                        ListTile(
+                            title: const Text('Counter'),
+                            onTap: () {
+                            // Route menu ke halaman utama
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(builder: (context) => const MyHomePage()),
+                            );
+                            },
+                        ),
+                        ListTile(
+                            title: const Text('Form'),
+                            onTap: () {
+                            // Route menu ke halaman form
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(builder: (context) => const MyFormPage()),
+                            );
+                            },
+                        ),
+                        ListTile(
+                            title: const Text('To Do'),
+                            onTap: () {
+                                // Route menu ke halaman to do
+                                Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(builder: (context) => const ToDoPage()),
+                                );
+                            },
+                        ),
                     ],
                 ),
             ),
